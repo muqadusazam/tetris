b18d45f42d2c0e729aabeb16897a41dbd403ad44
muqadusazam <30953671+muqadusazam@users.noreply.github.com>
2019-10-13 00:26:48 +1100

    commented block script

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,10 +5,11 @@ using System.Linq;
 
 public class Block : MonoBehaviour
 {
-    public List<Transform> blocks;
+    public List<Transform> blocks; // list of block prefabs to store
 
     private void Awake()
     {
+        // getting the list of blocks object present in the game and storing them in the list of block prefabs variable
         blocks = GetComponentsInChildren<Transform>().Where(x => x != this.transform).ToList();
     }
 }
