4f12f1cd520cc7bb89457b548780a8b1a4779658
muqadusazam <30953671+muqadusazam@users.noreply.github.com>
2019-10-13 00:05:04 +1100

    commented Auto Destroy script

--- Assets/Scripts/AutoDestroy.cs
+++ Assets/Scripts/AutoDestroy.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class AutoDestroy : MonoBehaviour
 {
-    public float maxLifetime;
-    float timeElapsed = 0;
+    public float maxLifetime; // maximum time of life
+    float timeElapsed = 0; // time gone since last frame
 
     void Update()
     {
-        timeElapsed += Time.deltaTime;
+        timeElapsed += Time.deltaTime; // incrementing time elapsed
 
-        if (timeElapsed >= maxLifetime)
-            GameObject.Destroy(this.gameObject);
+        if (timeElapsed >= maxLifetime) // checking if time elapsed is greater or equal to max life (means time gone beyond or equal to game end)
+            GameObject.Destroy(this.gameObject); // then detroy the game object
     }
 }
