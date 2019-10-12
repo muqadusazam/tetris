dd1c56639978ae1a0fce456df6a6dcf924961489
muqadusazam <30953671+muqadusazam@users.noreply.github.com>
2019-10-13 00:31:59 +1100

    commented button handler script

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -5,24 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class ButtonHandler : MonoBehaviour
 {
-    public void changeScene(string name)
+    public void changeScene(string name) // method to change the scene based on the button pressed
     {
-        if (name.Equals("end"))
+        if (name.Equals("end")) // if the button is "equal"
         {
-            Application.Quit();
+            Application.Quit(); // then exit out of the game
 
         }
-        else if (name.Equals("Original"))
+        else if (name.Equals("Original")) // if the button is "Original"
         {
-            SceneManager.LoadScene("Original", LoadSceneMode.Single);
+            SceneManager.LoadScene("Original", LoadSceneMode.Single); // then load the original game scene in a single load scene view
         }
-        else if (name.Equals("Modified"))
+        else if (name.Equals("Modified")) // if the button is "Modified"
         {
-            SceneManager.LoadScene("Modified", LoadSceneMode.Single);
+            SceneManager.LoadScene("Modified", LoadSceneMode.Single); // then load the modified game level in a single load scene view
         }
-        else if (name.Equals("MainMenu"))
+        else if (name.Equals("MainMenu")) // if the button is "MainMenu"
         {
-            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single); // then load the main menu game scene in a single load scene view
         }
 
     }
