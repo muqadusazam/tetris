1f0d7e1bf8d3c94c7665819b497f7c7420dae271
muqadusazam <30953671+muqadusazam@users.noreply.github.com>
2019-10-13 22:28:57 +1100

    fixed high scores

--- Assets/Scripts/highestScores.cs
+++ Assets/Scripts/highestScores.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class highestScores : MonoBehaviour
 {
+    public static int highestScoreValue = 0;
+    Text highScore;
     // Start is called before the first frame update
     void Start()
     {
-        
+        highScore = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (scores.scoreValue > highestScoreValue)
+        {
+            highestScoreValue = scores.scoreValue;
+            highScore.text = "" + highestScoreValue;
+        }
     }
 }
